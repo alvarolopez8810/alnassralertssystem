a90d3b6fc14aa724e526e1a790270000de28a9c0
alvarolopez8810 <alopezmolina4@gmail.com>
2026-01-22 09:17:01 +0100

    Add files via upload

--- config.py
+++ config.py
@@ -6,9 +6,7 @@ load_dotenv()
 EMAIL_USER = os.getenv('EMAIL_USER')
 EMAIL_PASSWORD = os.getenv('EMAIL_PASSWORD')
 EMAIL_JEFE = os.getenv('EMAIL_JEFE', 'al.scoutinglab@gmail.com')
-EMAIL_CHIEFSCOUT = os.getenv('EMAIL_CHIEFSCOUT', 'rafitagil@hotmail.com')
 
 EMAIL_DESTINATARIOS = [
-    EMAIL_JEFE,
-    EMAIL_CHIEFSCOUT
+    EMAIL_JEFE
 ]
